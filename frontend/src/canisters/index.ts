cb049d5c39cbf9c7b0c799f0b35d21e9d1c1888a
Deven Nathanael <devennatval@gmail.com>
2025-01-31 00:48:08 +0700

    Frontend W3T Canister

--- frontend/src/canisters/index.ts
+++ frontend/src/canisters/index.ts
@@ -1,11 +1,11 @@
 import { CandidCanister } from "@bundly/ares-core";
 
-import { TestActor, test } from "./test";
+import { W3TActor, w3t } from "./w3t";
 
 export type CandidActors = {
-  test: TestActor;
+  w3t: W3TActor;
 };
 
 export let candidCanisters: Record<keyof CandidActors, CandidCanister> = {
-  test,
+  w3t,
 };
