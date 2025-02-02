d86b5739d495ff06f28ca559c3584bcc7f9d0045
LAPTOP-JULIAN\Julian Yap <yjulian74@gmail.com>
2025-02-03 03:04:00 +0700

    Integrasi Approve Reject getMyBalance, refactor list-report,

--- frontend/src/pages/list-report/index.tsx
+++ frontend/src/pages/list-report/index.tsx
@@ -1,43 +1,118 @@
 import { Principal } from "@dfinity/principal";
-import { Box, Chip, Menu } from "@mantine/core";
+import { Box, Button, Chip, Group, Menu, Stack, TextInput } from "@mantine/core";
 import { modals } from "@mantine/modals";
 import dayjs from "dayjs";
 import { type MRT_ColumnDef, MantineReactTable, useMantineReactTable } from "mantine-react-table";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
-
-import { LogoutButton, useAuth, useCandidActor, useIdentities } from "@bundly/ares-react";
-
-import { CandidActors } from "@app/canisters";
 import DetailReport from "@app/components/DetailReport/DetailReport";
 import { GREEN_PRIMARY } from "@app/constants/colors";
-
 import type { Report, UidReport } from "../../declarations/w3t/w3t.did";
+import { useCanister } from "@app/contexts/CanisterContext";
+import { notifications } from "@mantine/notifications";
+import { IconX } from "@tabler/icons-react";
 
 const listReport = () => {
-  const { isAuthenticated, currentIdentity, changeCurrentIdentity } = useAuth();
+  const { w3tActor } = useCanister();
   const router = useRouter();
   const [role, setRole] = useState<string>("police");
-  const [reports, setReports] = useState<Report[] | undefined>(undefined);
-  const w3t = useCandidActor<CandidActors>("w3t", currentIdentity, {
-    canisterId: process.env.NEXT_PUBLIC_TEST_CANISTER_ID,
-  }) as CandidActors["w3t"];
+  const [reports, setReports] = useState<UidReport[] | undefined>(undefined);
+  // const [policeReportNumbers, setPoliceReportNumbers] = useState('');
 
   useEffect(() => {
-    getReports();
-  }, [currentIdentity]);
+    if(w3tActor){
+      getReports();
+    }
+  }, []);
 
   const openModalDetailPolice = ({ detailDataArray }: { detailDataArray: UidReport }) =>
     modals.openConfirmModal({
       title: "Detail Report",
       children: <DetailReport detailDataArray={detailDataArray} />,
       size: "lg",
-      confirmProps: { color: GREEN_PRIMARY },
-      labels: { confirm: "Approve", cancel: "Reject" },
-      onCancel: () => console.log("Cancel"),
-      onConfirm: () => console.log("Confirmed"),
+      closeOnConfirm: false,
+      confirmProps: "GuiltyWaitingForFineToBePaid" in detailDataArray[1].status ? {color: GREEN_PRIMARY} : "OnValidationProcess" in detailDataArray[1].status ? {color: GREEN_PRIMARY} : { display: "none" },
+      cancelProps: "OnValidationProcess" in detailDataArray[1].status ? {} : { display: "none" },
+      labels: "GuiltyWaitingForFineToBePaid" in detailDataArray[1].status ? { confirm: "Distribute Reward", cancel: "Reject" } : {confirm: "Approve", cancel: "Reject"},
+      onCancel: () => rejectFunc({uuid: detailDataArray[0]}),
+      onConfirm: () => {
+        if("OnValidationProcess" in detailDataArray[1].status){
+            modals.openConfirmModal({
+              title: "Police Report",
+              closeOnConfirm: false,
+              children: (
+                <PoliceReportModal
+                  uuid={detailDataArray[0]}
+                />
+              ),
+              cancelProps: { display: "none" },
+              confirmProps: { display: "none" },
+              labels: {confirm: "Approve", cancel: "Reject"}
+            })
+        }else{
+          console.log("Confirmed")
+        }
+      },
     });
 
+    const PoliceReportModal = ({uuid} : {uuid: string}) => {
+      const [policeReportNumbers, setPoliceReportNumbers] = useState('');
+  
+      return (
+        <Stack>
+          <TextInput 
+            label="Police Report Number" 
+            onChange={(e) => {
+              setPoliceReportNumbers(e.currentTarget.value);
+            }} 
+            value={policeReportNumbers} 
+            withAsterisk 
+          />
+          <Group justify="flex-end" mt={40}>
+            <Button color={GREEN_PRIMARY} onClick={() => {
+              if (policeReportNumbers === "") {
+                notifications.show({
+                  title: "Error!",
+                  message: "Please Fill Police Report Number!",
+                  color: "red",
+                  icon: <IconX />
+                });
+              } else {
+                approveFunc({uuid: uuid})
+              }
+            }}>Approve</Button>
+
+          </Group>
+        </Stack>
+      );
+    };
+
+    const approveFunc = async({uuid} : {uuid: string}) => {
+      try {
+        const response = await w3tActor.validateReportStatus([uuid, { "GuiltyWaitingForFineToBePaid" : null}]);
+        modals.closeAll();
+      } catch (error: any) {
+        notifications.show({
+          title: "Error!",
+          message: error,
+          color: "red",
+          icon: <IconX />
+        })
+      }
+    }
+    const rejectFunc = async({uuid} : {uuid: string}) => {
+      try {
+        const response = await w3tActor.validateReportStatus([uuid, { "NotGuilty" : null}]);
+        modals.closeAll();
+      } catch (error: any) {
+        notifications.show({
+          title: "Error!",
+          message: error,
+          color: "red",
+          icon: <IconX />
+        })
+      }
+    }
   const openModalDetailUser = ({ detailDataArray }: { detailDataArray: UidReport }) =>
     modals.open({
       title: "Detail Report",
@@ -47,18 +122,21 @@ const listReport = () => {
 
   async function getReports() {
     try {
-      const response = await w3t.getAllReports();
-      console.log(currentIdentity);
+      const response = await w3tActor.getAllReports();
       if ("err" in response) {
         if ("userNotAuthorized" in response.err) console.log("User Not Authorized");
         else console.log("Error fetching Report");
       }
 
       const reportList: any = "ok" in response ? response.ok : undefined;
-      console.log(reportList);
       setReports(reportList);
-    } catch (error) {
-      console.error({ error });
+    } catch (error:any) {
+      notifications.show({
+        title: "Error!",
+        message: error,
+        color: "red",
+        icon: <IconX />
+      })
     }
   }
   // Example Principal instances
@@ -98,6 +176,29 @@ const listReport = () => {
         submittedAt: [BigInt(Date.now())],
         policeReportNumber: ["PR67890"],
         licenseNumber: "XYZ789",
+        validatedAt: [],
+        reporter: examplePrincipal2,
+        violationType: {
+          chapter: BigInt(287),
+          clause: BigInt(1),
+          fine: BigInt(500000),
+          briefDescription: "Melanggar Marka Jalan",
+          completeDescription:
+            "Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan yang melanggar aturan perintah atau larangan yang dinyatakan dengan Rambu Lalu Lintas sebagaimana dimaksud dalam Pasal 106 ayat (4) huruf a atau Marka Jalan sebagaimana dimaksud dalam Pasal 106 ayat (4) huruf b dipidana dengan pidana kurungan paling lama 2 (dua) bulan atau denda paling banyak Rp500.000,00 (lima ratus ribu rupiah).",
+        },
+        police: [examplePrincipal1],
+      },
+    ],
+    [
+      "iniuuid3",
+      {
+        status: { GuiltyWaitingForFineToBePaid: null },
+        rewardAmount: BigInt(2000),
+        rewardPaidAt: [],
+        stakeAmount: BigInt(750),
+        submittedAt: [BigInt(Date.now())],
+        policeReportNumber: ["PR67890"],
+        licenseNumber: "XYZ729",
         validatedAt: [],
         reporter: examplePrincipal2,
         violationType: {
@@ -211,7 +312,6 @@ const listReport = () => {
     enablePagination: false,
     enableRowActions: true,
     renderRowActionMenuItems: ({ row }: { row: any }) => {
-      console.log(row.original);
       return (
         <>
           <Menu.Item onClick={() => openModalDetailPolice({ detailDataArray: row.original })}>
@@ -229,7 +329,6 @@ const listReport = () => {
     enablePagination: false,
     enableRowActions: true,
     renderRowActionMenuItems: ({ row }: { row: any }) => {
-      console.log(row.original);
       return (
         <>
           <Menu.Item onClick={() => openModalDetailUser({ detailDataArray: row.original })}>Detail</Menu.Item>
