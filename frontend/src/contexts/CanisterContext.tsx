c12fa409bcbf6d4fd327329185e2bf8dbce88e48
Deven Nathanael <devennatval@gmail.com>
2025-02-03 12:08:28 +0700

    Add init role, disconnect and refactor

--- frontend/src/contexts/CanisterContext.tsx
+++ frontend/src/contexts/CanisterContext.tsx
@@ -3,8 +3,10 @@ import { idlFactory } from "../declarations/w3t/w3t.did.js";
 
 interface CanisterContextType {
   principalId: string;
+  role: string;
   w3tActor: any;
   requestConnect: () => Promise<void>;
+  disconnect: () => Promise<void>;
 }
 
 const CanisterContext = createContext<CanisterContextType | undefined>(undefined);
@@ -12,32 +14,27 @@ const CanisterContext = createContext<CanisterContextType | undefined>(undefined
 export const CanisterProvider = ({ children }: { children: ReactNode }) => {
   const [principalId, setPrincipalId] = useState("");
   const [w3tActor, setW3tActor] = useState<any>(null);
-  let plugExtension:any = null;
-
-  useEffect(() => {
-    plugExtension = window.ic?.plug
-  }, [])
+  const [role, setRole] = useState("User");
   
   const w3tCanisterId = process.env.NEXT_PUBLIC_W3T_CANISTER_ID ?? "";
+  
+  useEffect(() => {
+    if(w3tActor !== null) {
+      initRole();
+    }
+  }, [w3tActor]);
 
   const requestConnect = async () => {
+    let plugExtension = window?.ic?.plug;
+
     if(!!plugExtension) {
       try {
         const whitelist: [string] = [w3tCanisterId];
         const isConnected = await plugExtension.isConnected();
-
         if (!isConnected) await plugExtension.requestConnect({ whitelist });
-
-        await plugExtension.createAgent({ whitelist });
-
-        // Init actor
-        const NNSUiActor = await plugExtension.createActor({
-          canisterId: w3tCanisterId,
-          interfaceFactory: idlFactory,
-        }) as any;
-        setW3tActor(NNSUiActor);
+        await initW3tActor();
         setPrincipalId(plugExtension.principalId);
-        
+
       } catch (error) {
         console.log("Failed to connect to canister:", error);
       }
@@ -47,7 +44,38 @@ export const CanisterProvider = ({ children }: { children: ReactNode }) => {
     }
   };
 
-  return <CanisterContext.Provider value={{ principalId, w3tActor, requestConnect }}>{children}</CanisterContext.Provider>;
+  const initW3tActor = async (): Promise<any> => {
+    let plugExtension = window?.ic?.plug;
+    const NNSUiActor = await plugExtension!.createActor({
+      canisterId: w3tCanisterId,
+      interfaceFactory: idlFactory,
+    }) as any;
+    setW3tActor(NNSUiActor);
+  }
+
+  const initRole = async (): Promise<void> => {
+    const role = await w3tActor.getMyRole();
+    setRole(role);
+  }
+
+  const disconnect = async () => {
+    let plugExtension = window?.ic?.plug;
+
+    try {
+      await plugExtension!.disconnect();
+      resetState();
+    } catch(error) {
+      console.log("Failed to disconnect to canister:", error);
+    }
+  }; 
+
+  const resetState = () => {
+    setW3tActor(null);
+    setPrincipalId("");
+    setRole("User");
+  };
+
+  return <CanisterContext.Provider value={{ principalId, role, w3tActor, requestConnect, disconnect }}>{children}</CanisterContext.Provider>;
 };
 
 export const useCanister = () => {
