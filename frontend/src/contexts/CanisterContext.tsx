a18b98aa221e939d2dd339730f48904ba90a2f4e
Deven Nathanael <devennatval@gmail.com>
2025-02-03 18:34:42 +0700

    Add Deposit and Withdraw Function on CanisterContext

--- frontend/src/contexts/CanisterContext.tsx
+++ frontend/src/contexts/CanisterContext.tsx
@@ -1,12 +1,17 @@
 import { createContext, useContext, useState, ReactNode, useEffect } from "react";
-import { idlFactory } from "../declarations/w3t/w3t.did.js";
+import { idlFactory as w3tIF } from "../declarations/w3t/w3t.did.js";
+import { TransferArg, idlFactory as w3tTokenIF } from "../declarations/w3t_token/w3t_token.did.js";
+import { Principal } from "@dfinity/principal";
 
 interface CanisterContextType {
   principalId: string;
   role: string;
   w3tActor: any;
+  tokenActor: any;
   requestConnect: () => Promise<void>;
   disconnect: () => Promise<void>;
+  depositToken: (amount: number) => Promise<void>; 
+  withdrawToken: (amount: number) => Promise<void>; 
 }
 
 const CanisterContext = createContext<CanisterContextType | undefined>(undefined);
@@ -14,9 +19,11 @@ const CanisterContext = createContext<CanisterContextType | undefined>(undefined
 export const CanisterProvider = ({ children }: { children: ReactNode }) => {
   const [principalId, setPrincipalId] = useState("");
   const [w3tActor, setW3tActor] = useState<any>(null);
+  const [tokenActor, setTokenActor] = useState<any>(null);
   const [role, setRole] = useState("User");
   
   const w3tCanisterId = process.env.NEXT_PUBLIC_W3T_CANISTER_ID ?? "";
+  const w3tTokenCanisterId = process.env.NEXT_PUBLIC_W3T_TOKEN_CANISTER_ID ?? "";
   
   useEffect(() => {
     if(w3tActor !== null) {
@@ -29,10 +36,14 @@ export const CanisterProvider = ({ children }: { children: ReactNode }) => {
 
     if(!!plugExtension) {
       try {
-        const whitelist: [string] = [w3tCanisterId];
+        const whitelist: string[] = [w3tCanisterId, w3tTokenCanisterId];
         const isConnected = await plugExtension.isConnected();
         if (!isConnected) await plugExtension.requestConnect({ whitelist });
-        await initW3tActor();
+        await Promise.all([
+          initW3tActor(),
+          initTokenActor()
+        ])
+        
         setPrincipalId(plugExtension.principalId);
 
       } catch (error) {
@@ -48,14 +59,25 @@ export const CanisterProvider = ({ children }: { children: ReactNode }) => {
     let plugExtension = window?.ic?.plug;
     const NNSUiActor = await plugExtension!.createActor({
       canisterId: w3tCanisterId,
-      interfaceFactory: idlFactory,
+      interfaceFactory: w3tIF,
     }) as any;
     setW3tActor(NNSUiActor);
+    console.log("Successfully connect to w3t actor");
   }
 
   const initRole = async (): Promise<void> => {
     const role = await w3tActor.getMyRole();
     setRole(role);
+  }
+
+  const initTokenActor = async (): Promise<any> => {
+    let plugExtension = window?.ic?.plug;
+    const NNSUiActor = await plugExtension!.createActor({
+      canisterId: w3tTokenCanisterId,
+      interfaceFactory: w3tTokenIF,
+    }) as any;
+    setTokenActor(NNSUiActor);
+    console.log("Successfully connect to token actor");
   }
 
   const disconnect = async () => {
@@ -75,7 +97,41 @@ export const CanisterProvider = ({ children }: { children: ReactNode }) => {
     setRole("User");
   };
 
-  return <CanisterContext.Provider value={{ principalId, role, w3tActor, requestConnect, disconnect }}>{children}</CanisterContext.Provider>;
+  const depositToken = async (amount: number) => {
+    try {
+      const args: TransferArg = {
+        to: {
+          owner: Principal.fromText(w3tCanisterId),
+          subaccount: [],
+        },
+        amount: BigInt(amount),
+        fee: [],
+        memo: [],
+        from_subaccount: [],
+        created_at_time: [],
+      }
+
+      const transferRes = await tokenActor.icrc1_transfer(args);
+      if("ok" in transferRes) {
+        await w3tActor.deposit(amount);
+        console.log("Deposit succeed:", transferRes.ok);
+      } else if("err" in transferRes) console.log("Failed to deposit token:", transferRes.err);
+    } catch (error) {
+      console.log("Failed to deposit token:", error);
+    }
+  }
+
+  const withdrawToken = async (amount: number) => {
+    try {
+      const transferRes = await w3tActor.withdrawToken(amount);
+      if("ok" in transferRes) console.log("Withdraw succeed:", transferRes.ok);
+      else if('err' in transferRes) console.log("Failed to withdraw token:", transferRes.err);
+    } catch (error) {
+      console.log("Failed to withdraw token:", error);
+    }
+  }
+
+  return <CanisterContext.Provider value={{ principalId, role, w3tActor, tokenActor, requestConnect, disconnect, depositToken, withdrawToken }}>{children}</CanisterContext.Provider>;
 };
 
 export const useCanister = () => {
